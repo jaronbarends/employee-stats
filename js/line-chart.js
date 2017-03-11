05b04b50508773ff117843c81405d1d106f7cdd0
jaronbarends <jaron.barends@valtech.nl>
2017-03-11 23:31:10 +0100

    cleaned up line chart code

--- js/line-chart.js
+++ js/line-chart.js
@@ -4,58 +4,92 @@ app.lineChart = (function($) {
 
 	'use strict';
 
+	var sgChartGroup,
+		sgWidth,
+		sgHeight,
+		sgXScale,
+		sgYScale,
+		sgTotalData,
+		sgStartedData;
+
+
+
+	/**
+	* initialize chart
+	* @returns {undefined}
+	*/
+	var initChart = function() {
+		var lineChart = d3.select("#line-chart"),
+			svgWidth = parseInt(lineChart.style("width"), 10),
+			svgHeight = parseInt(lineChart.style("height"), 10),
+			margin = {top: 20, right: 20, bottom: 30, left: 50};
+
+		sgWidth = svgWidth - margin.left - margin.right,
+		sgHeight = svgHeight - margin.top - margin.bottom;
+
+		sgChartGroup = lineChart.append("g").attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+	};
+	
+
+
 	/**
 	* 
 	* @returns {undefined}
 	*/
-	var init = function() {
-		var lineChart = d3.select("#line-chart"),
-			svgWidth = parseInt(lineChart.style("width"), 10),
-			svgHeight = parseInt(lineChart.style("height"), 10),
-			margin = {top: 20, right: 20, bottom: 30, left: 50},
-			width = svgWidth - margin.left - margin.right,
-			height = svgHeight - margin.top - margin.bottom,
-			g = lineChart.append("g").attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-
+	var initData = function() {
 		var parseTime = d3.timeParse('%d/%m/%Y');
 
-		var xScale = d3.scaleTime()
-				.rangeRound([0, width]);
-
-		var yScale = d3.scaleLinear()
-				.rangeRound([height, 0]);
-
-		var line = d3.line()
-				.curve(d3.curveCardinal)
-				.x(function(d) { return xScale(d.year); })
-				.y(function(d) { return yScale(d.count); }),
-			line2 = d3.line()
-				.curve(d3.curveCardinal)
-				.x(function(d) { return xScale(d.year); })
-				.y(function(d) { return yScale(d.countCumulative); });
-
-		var totalData = app.data.employeesPerYear,
-			startedData = app.data.employeesStartedPerYear;
+		sgTotalData = app.data.employeesPerYear;
+		sgStartedData = app.data.employeesStartedPerYear;
 		
 		 // process data for totals
-		for (var i=0; i<totalData.length; i++) {
-			var d = totalData[i];
+		for (var i=0; i<sgTotalData.length; i++) {
+			var d = sgTotalData[i];
 			d.year = parseTime(d.year);
 			d.count = +d.count;
 		}
+	};
 
-		xScale.domain(d3.extent(totalData, function(d) { return d.year; }));
-		yScale.domain(d3.extent(totalData, function(d) { return d.count; }));
-		yScale.domain([0, d3.max(totalData, function(d) { return d.count; })]);
 
-		g.append("g")
+
+	/**
+	* 
+	* @returns {undefined}
+	*/
+	var initScales = function() {
+		// first determine the extent of our datasets
+		var totalYearExtent = d3.extent(sgTotalData, function(d) { return d.year; }),
+			totalCountExtent = d3.extent(sgTotalData, function(d) { return d.count; }),
+			startedYearExtent = d3.extent(sgStartedData, function(d) { return d.year; }),
+			startedCountExtent = d3.extent(sgStartedData, function(d) { return d.countCumulative; }),
+			xMin = Math.min(totalYearExtent[0], startedYearExtent[0]),
+			xMax = Math.max(totalYearExtent[1], startedYearExtent[1]),
+			yMax = Math.max(totalCountExtent[1], startedCountExtent[1]);
+
+		// now create scales
+		sgXScale = d3.scaleTime()
+			.rangeRound([0, sgWidth])
+			.domain([xMin, xMax]);
+
+		sgYScale = d3.scaleLinear()
+			.rangeRound([sgHeight, 0])
+			.domain([0, yMax]);
+	};
+
+
+	/**
+	* 
+	* @returns {undefined}
+	*/
+	var createAxes = function() {
+		sgChartGroup.append("g")
 			.attr('class', 'axis--bottom')
-			.attr("transform", "translate(0," + height + ")")
-			.call(d3.axisBottom(xScale));
+			.attr("transform", "translate(0," + sgHeight + ")")
+			.call(d3.axisBottom(sgXScale));
 
-		g.append("g")
+		sgChartGroup.append("g")
 			.attr('class', 'axis--left')
-			.call(d3.axisLeft(yScale))
+			.call(d3.axisLeft(sgYScale))
 			.append("text")
 			.attr("fill", "#000")
 			.attr("transform", "rotate(-90)")
@@ -63,43 +97,78 @@ app.lineChart = (function($) {
 			.attr("dy", "0.71em")
 			.attr("text-anchor", "end")
 			.text("Number of employees");
+	};
+	
+		
 
-		g.append("path")
-			.datum(totalData)
-			.attr('class', 'chart-path')
+	/**
+	* draw a line
+	* @param {object} dataset The dataset to use for this line
+	* @param {yProp} string	The dataset-property to use for the y-value
+	* @param {string} additionalClasses Additional classes to add to the path
+	* @returns {undefined}
+	*/
+	var drawLine = function(dataset, yProp, additionalClasses) {
+		var cls = 'chart-path';
+		if (additionalClasses) {
+			cls += ' '+additionalClasses;
+		}
+
+		var line = d3.line()
+				.curve(d3.curveCardinal)
+				.x(function(d) { return sgXScale(d.year); })
+				.y(function(d) { return sgYScale(d[yProp]); });
+
+		sgChartGroup.append("path")
+			.datum(dataset)
+			.attr('class', cls)
 			.attr("d", line);
+	};
+	
+		
 
-		 g.append('g')
-		 	.selectAll('circle')
-		 	.data(totalData)
-		 	.enter()
-		 	.append('circle')
-		 	.attr('cx', function(d) {
-		 		return xScale(d.year);
-		 	})
-		 	.attr('cy', function(d) {
-		 		return yScale(d.count);
-		 	});
+	/**
+	* draw markers
+	* @param {object} dataset The dataset to use for these markers
+	* @param {yProp} string	The dataset-property to use for the y-value
+	* @param {string} additionalClasses Additional classes to add to the group
+	* @returns {undefined}
+	*/
+	var drawMarkers = function(dataset, yProp, additionalClasses) {
+		var cls = 'chart-path';
+		if (additionalClasses) {
+			cls += ' '+additionalClasses;
+		}
 
-		 // now add data for start year
-		 g.append('path')
-		 	.datum(startedData)
-		 	.attr('class', 'chart-path secondary')
-		 	.attr('d', line2);
+		sgChartGroup.append('g')
+			.attr('class', cls)
+			.selectAll('circle')
+			.data(dataset)
+			.enter()
+			.append('circle')
+			.attr('cx', function(d) {
+				return sgXScale(d.year);
+			})
+			.attr('cy', function(d) {
+				return sgYScale(d[yProp]);
+			});
+	};
+	
 
-		 g.append('g')
-		 	.attr('class', 'secondary')
-		 	.selectAll('circle')
-		 	.data(startedData)
-		 	.enter()
-		 	.append('circle')
-		 	.attr('cx', function(d) {
-		 		return xScale(d.year);
-		 	})
-		 	.attr('cy', function(d) {
-		 		return yScale(d.countCumulative);
-		 	})
-		 	.attr('r', 5);
+	/**
+	* initialize the chart
+	* @returns {undefined}
+	*/
+	var init = function() {
+		initChart();
+		initData();
+		initScales();
+		createAxes();
+
+		drawLine(sgTotalData, 'count');
+		drawMarkers(sgTotalData, 'count');
+		drawLine(sgStartedData, 'countCumulative', 'secondary');
+		drawMarkers(sgStartedData, 'countCumulative', 'secondary');
 	};
 
 
